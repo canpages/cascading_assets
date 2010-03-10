f3ac35dee7de4fde2ae07e20e7404486ed8fc678
Angelo B <git-email@angelo.ca>
2010-03-10 01:52:51 -0500

    Remove checking for Less

--- lib/cascading_assets.rb
+++ lib/cascading_assets.rb
@@ -1,29 +1,24 @@
 # CascadingAssets
 
 module ActionView::Helpers::AssetTagHelper
-private
-    def expand_javascript_sources_with_cascade(sources, recursive = false)
-        expand_cascading_sources(sources, ActionView::Helpers::AssetTagHelper::JAVASCRIPTS_DIR, "js")
-        expand_javascript_sources_without_cascade(sources, recursive)
-    end
-    alias_method_chain :expand_javascript_sources, :cascade
+  private
+  def expand_javascript_sources_with_cascade(sources, recursive = false)
+    expand_cascading_sources(sources, ActionView::Helpers::AssetTagHelper::JAVASCRIPTS_DIR, "js")
+    expand_javascript_sources_without_cascade(sources, recursive)
+  end
+  alias_method_chain :expand_javascript_sources, :cascade
 
-    def expand_stylesheet_sources_with_cascade(sources, recursive = false)
-        expand_cascading_sources(sources, ActionView::Helpers::AssetTagHelper::STYLESHEETS_DIR, "css")
-        expand_stylesheet_sources_without_cascade(sources, recursive)
-    end
-    alias_method_chain :expand_stylesheet_sources, :cascade
+  def expand_stylesheet_sources_with_cascade(sources, recursive = false)
+    expand_cascading_sources(sources, ActionView::Helpers::AssetTagHelper::STYLESHEETS_DIR, "css")
+    expand_stylesheet_sources_without_cascade(sources, recursive)
+  end
+  alias_method_chain :expand_stylesheet_sources, :cascade
 
-    def expand_cascading_sources(sources, dir, extension)
-        if sources.delete(:cascades)
-            ['application', @controller.active_layout.try(:name), @controller.controller_name, "#{@controller.controller_name}/#{@controller.action_name}"].compact.uniq.each do |source|
-                if File.exists?(File.join(Less::More.source_path,"#{source}.less")) && extension == "css"
-                    dir = Less::More.destination_path
-                    sources << File.join('', dir, "#{source}.#{extension}")
-                else
-                    sources << source if File.exists?(File.join(dir, "#{source}.#{extension}"))
-                end
-            end
-        end
+  def expand_cascading_sources(sources, dir, extension)
+    if sources.delete(:cascades)
+      ['application', @controller.active_layout.try(:name), @controller.controller_name, "#{@controller.controller_name}/#{@controller.action_name}"].compact.uniq.each do |source|
+        sources << source if File.exists?(File.join(dir, "#{source}.#{extension}"))
+      end
     end
+  end
 end
