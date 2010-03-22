de612976af85adf782dab66f17cd3c431851bf94
Angelo B <git-email@angelo.ca>
2010-03-22 15:24:46 -0400

    Include application asset as part of :cascades, not :defaults.  (Inconsistent behaviour here in Rails between default stylesheets and default javascripts)

--- lib/cascading_assets.rb
+++ lib/cascading_assets.rb
@@ -2,10 +2,21 @@
 
 module ActionView::Helpers::AssetTagHelper
   private
+
+
   def expand_javascript_sources_with_cascade(sources, recursive = false)
     expand_cascading_sources(sources, ActionView::Helpers::AssetTagHelper::JAVASCRIPTS_DIR, "js")
-    expand_javascript_sources_without_cascade(sources, recursive)
+    if sources.include?(:all)
+      all_javascript_files = collect_asset_files(JAVASCRIPTS_DIR, ('**' if recursive), '*.js')
+      ((determine_source(:defaults, @@javascript_expansions).dup & all_javascript_files) + all_javascript_files).uniq
+    else
+      expanded_sources = sources.collect do |source|
+        determine_source(source, @@javascript_expansions)
+      end.flatten
+      expanded_sources
+    end
   end
+
   alias_method_chain :expand_javascript_sources, :cascade
 
   def expand_stylesheet_sources_with_cascade(sources, recursive = false)
@@ -16,7 +27,7 @@ module ActionView::Helpers::AssetTagHelper
 
   def expand_cascading_sources(sources, dir, extension)
     if sources.delete(:cascades)
-      [@controller.active_layout.try(:name), @controller.controller_name, "#{@controller.controller_name}/#{@controller.action_name}"].compact.uniq.each do |source|
+      ['application', @controller.active_layout.try(:name), @controller.controller_name, "#{@controller.controller_name}/#{@controller.action_name}"].compact.uniq.each do |source|
         sources << source if File.exists?(File.join(dir, "#{source}.#{extension}"))
       end
     end
